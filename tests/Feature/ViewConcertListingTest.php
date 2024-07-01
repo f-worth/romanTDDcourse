f1ff4b2f2e2adc2e1aa18b88ca6dce46a0b6ec7a
f-worth <166548370+f-worth@users.noreply.github.com>
2024-07-01 14:19:35 -0500

    Created Repo

---
+++ tests/Feature/ViewConcertListingTest.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Tests\Feature;
+
+// use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class ViewConcertListingTest extends TestCase
+{
+    /** @test */
+    public function user_can_view_a_concert_listing()
+    {
+        // Arrange
+
+            // Create a concert
+
+        // Act
+        
+            // View the concert listing
+
+        // Assert
+
+            // See the concert details
+    }
+}
